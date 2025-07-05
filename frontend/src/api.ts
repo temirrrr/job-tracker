b69fdfecdcb168c97ba5ce50f911abe2cf85d3de
Tryth8 <zharylgamysov@gmail.com>
2025-07-05 14:18:58 +0500

    chore: point API to production backend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://job-tracker-production-94bb.up.railway.app/docs',
 });
 
 API.interceptors.request.use(config => {
